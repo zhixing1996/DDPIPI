caa293ca67fa26fae88a23a8af13bffdd982bd74
zhixing1996 <jingmaoqiang1996@163.com>
2019-08-06 09:17:16 +0800

    fix path bugs(v0.1)

--- init-boss.sh
+++ init-boss.sh
@@ -6,10 +6,10 @@ source setupCMT.csh
 
 cmt config
 
-cd $HOME/bes/DDPIPI/TestRelease/TestRelease-00-00-80/cmt
+cd $HOME/bes/DDPIPI/v0.1/TestRelease/TestRelease-00-00-80/cmt
 
 cmt br cmt config
 
 cmt br gmake
 
-cd $HOME/bes/DDPIPI
+cd $HOME/bes/DDPIPI/v0.1
