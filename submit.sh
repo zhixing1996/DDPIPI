609e1264178129bc583a38c213dcb80448da440d
zhixing1996 <jingmaoqiang1996@163.com>
2019-08-12 12:15:37 +0800

    add 4600 section

--- submit.sh
+++ submit.sh
@@ -16,6 +16,9 @@ usage() {
     printf "\n\t%-9s  %-40s\n" "0.2"   "[run on signal MC of psi(4415)->D1_(2420)D(PHSP), D1_(2420)->DPIPI(PHSP) @4420MeV]" # psi(4415) -> @4420MeV
     printf "\n\t%-9s  %-40s\n" "0.2.1" "Simulation & Reconstruction -- generate signal MC sample"
     printf "\n\t%-9s  %-40s\n" "0.2.2" "Single D tag -- run on signal MC sample"
+    printf "\n\t%-9s  %-40s\n" "0.3"   "[run on signal MC of psi(4415)->D1_(2420)D(PHSP), D1_(2420)->DPIPI(PHSP) @4600MeV]" # psi(4415) -> @4600MeV
+    printf "\n\t%-9s  %-40s\n" "0.3.1" "Simulation & Reconstruction -- generate signal MC sample"
+    printf "\n\t%-9s  %-40s\n" "0.3.2" "Single D tag -- run on signal MC sample"
     printf "\n\t%-9s  %-40s\n" ""      ""
     printf "\n\n"
 }
@@ -115,6 +118,50 @@ case $option in
            ./makeJob.csh Sig_D1_2420_D_PHSP_4420 20 20 . sigMC D1_2420 PHSP 4420
            cp -rf $HOME/bes/DDPIPI/v0.1/scripts/gen_script/gen_mc/subjectAna.sh ./
            ./subjectAna.sh Sig_D1_2420_D_PHSP_4420 0 99
+           ;;
+
+    # ------------------------------------------------------------------------------
+    #  0.3 signal MC of psi(4415)->D_1(2420)D(PHSP), D_1(2420)->DPIPI(PHSP) @4600MeV
+    # ------------------------------------------------------------------------------
+
+    0.3) echo "signal MC of psi(4415)->D1_(2420)D(PHSP), D1_(2420)->DPIPI(PHSP) @4600MeV..."
+         echo "--> Process: psi(4415)->D1_(2420)D, D1_(2420)->DPIPI"
+         echo "--> E_{CMS}: 4600MeV"
+         echo "--> Generation mode: psi(4415)->D1_(2420)D PHSH; D1_(2420)->DPiPi PHSP; D decay D_DALITZ(assignated mode) or PHSP(inlusive mode)"
+         echo "--> Event Number: 1,000,000"
+         echo "--> RunNo: 35227~35411"
+         ;;
+
+    0.3.1) echo "Simulation & Reconstruction -- generate signal MC sample..."
+           mkdir -p scripts/sigMC/D1_2420/4600
+           cd scripts/sigMC/D1_2420/4600
+           if [ ! -d "/besfs/groups/tauqcd/$USER/bes/DDPIPI/v0.1/run/gen_mc/sigMC/D1_2420/4600/jobs_sig" ]; then
+               mkdir -p /besfs/groups/tauqcd/$USER/bes/DDPIPI/v0.1/run/gen_mc/sigMC/D1_2420/4600/jobs_sig
+               ln -s /besfs/groups/tauqcd/$USER/bes/DDPIPI/v0.1/run/gen_mc/sigMC/D1_2420/4600/jobs_sig ./jobs_sig
+           fi
+           cd jobs_sig
+           mkdir -p /scratchfs/bes/$USER/bes/DDPIPI/v0.1/sigMC/D1_2420/4600/rtraw
+           mkdir -p /scratchfs/bes/$USER/bes/DDPIPI/v0.1/sigMC/D1_2420/4600/dst
+           rm -rf jobOptions*txt
+           rm -rf subjectSimRec_*.sh
+           cp -rf $HOME/bes/DDPIPI/v0.1/scripts/gen_script/gen_mc/jobOptions_sim_sig_D1_2420_D_PHSP_4600.sh ./
+           rm -rf /scratchfs/bes/$USER/bes/DDPIPI/v0.1/sigMC/D1_2420/4600/rtraw/*.rtraw
+           ./jobOptions_sim_sig_D1_2420_D_PHSP_4600.sh 0 99 10000
+           cp -rf $HOME/bes/DDPIPI/v0.1/scripts/gen_script/gen_mc/jobOptions_rec_sig_D1_2420_D_PHSP_4600.sh ./
+           rm -rf /scratchfs/bes/$USER/bes/DDPIPI/v0.1/sigMC/D1_2420/4600/dst/*.dst
+           ./jobOptions_rec_sig_D1_2420_D_PHSP_4600.sh 0 99
+           cp -rf $HOME/bes/DDPIPI/v0.1/scripts/gen_script/gen_mc/subjectSimRec.sh ./
+           ./subjectSimRec.sh jobOptions_sim_sig_D1_2420_D_PHSP_4600 jobOptions_rec_sig_D1_2420_D_PHSP_4600 subjectSimRec_PHSP 0 99
+           ;;
+
+    0.3.2) echo "Single D tag -- run on signal MC sample..."
+           mkdir -p /besfs/users/$USER/DDPIPI/v0.1/sigMC/D1_2420/4600
+           cd scripts/sigMC/D1_2420/4600/jobs_sig
+           rm -rf dstlist.txt
+           cp -rf $HOME/bes/DDPIPI/v0.1/scripts/gen_script/gen_mc/makeJob* ./
+           ./makeJob.csh Sig_D1_2420_D_PHSP_4600 20 20 . sigMC D1_2420 PHSP 4600
+           cp -rf $HOME/bes/DDPIPI/v0.1/scripts/gen_script/gen_mc/subjectAna.sh ./
+           ./subjectAna.sh Sig_D1_2420_D_PHSP_4600 0 99
            ;;
 
 esac
