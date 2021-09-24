b128f3df066d515f4078c6692f77040a6f9b83ba
zhixing1996 <jingmaoqiang1996@163.com>
2021-09-24 10:28:03 +0800

    sys_err: D1_2420_shape

--- python/sys_err/D1_2420_shape/analysis.sh
+++ python/sys_err/D1_2420_shape/analysis.sh
@@ -13,10 +13,12 @@ usage() {
     printf "\n\t%-9s  %-40s\n" "0.1"   "[Pretreatment of data and MC samples]"
     printf "\n\t%-9s  %-40s\n" "0.1.1" "Simulation & Reconstruction -- generate D1_2420 MC sample(@703p01)"
     printf "\n\t%-9s  %-40s\n" "0.1.2" "Simulation & Reconstruction -- generate D1_2420 MC sample(@705)"
-    printf "\n\t%-9s  %-40s\n" "0.1.3" "Single D tag -- run on D1_2420 MC sample(@703p01)"
-    printf "\n\t%-9s  %-40s\n" "0.1.4" "Single D tag -- run on D1_2420 MC sample(@705)"
-    printf "\n\t%-9s  %-40s\n" "0.1.5" "Get samples -- extracte useful info: signal region and sideband region, ISR factors"
-    printf "\n\t%-9s  %-40s\n" "0.1.6" "Get samples -- apply cuts: D1_2420 MC"
+    printf "\n\t%-9s  %-40s\n" "0.1.3" "Simulation & Reconstruction -- generate D1_2420 MC sample(@707)"
+    printf "\n\t%-9s  %-40s\n" "0.1.4" "Single D tag -- run on D1_2420 MC sample(@703p01)"
+    printf "\n\t%-9s  %-40s\n" "0.1.5" "Single D tag -- run on D1_2420 MC sample(@705)"
+    printf "\n\t%-9s  %-40s\n" "0.1.6" "Single D tag -- run on D1_2420 MC sample(@707)"
+    printf "\n\t%-9s  %-40s\n" "0.1.7" "Get samples -- extracte useful info: signal region and sideband region, ISR factors"
+    printf "\n\t%-9s  %-40s\n" "0.1.8" "Get samples -- apply cuts: D1_2420 MC"
 
     printf "\n\t%-9s  %-40s\n" "0.2"   "[Get system uncertainties]"
     printf "\n\t%-9s  %-40s\n" "0.2.1" "Fit distributions -- simultaneous fit recoiling mass of D + Dmiss and recoiling mass of pipi"
@@ -58,24 +60,34 @@ case $option in
            ./subSimRec_D1_2420_705.sh
            ;;
 
-    0.1.3) echo "Single D tag -- running on D1_2420 MC sample(@703p01)..."
+    0.1.3) echo "Simulation & Reconstruction -- generating D1_2420 MC sample(@707)..."
+           cd scripts/mc/D1_2420
+           ./subSimRec_D1_2420_707.sh
+           ;;
+
+    0.1.4) echo "Single D tag -- running on D1_2420 MC sample(@703p01)..."
            cd scripts/mc/D1_2420
            ./subAna_D1_2420_703p01.sh
            ;;
 
-    0.1.4) echo "Single D tag -- running on D1_2420 MC sample(@705)..."
+    0.1.5) echo "Single D tag -- running on D1_2420 MC sample(@705)..."
            cd scripts/mc/D1_2420
            ./subAna_D1_2420_705.sh
            ;;
 
-    0.1.5) echo "Get samples -- extracting useful info: signal region and sideband region, ISR factors..."
+    0.1.6) echo "Single D tag -- running on D1_2420 MC sample(@707)..."
+           cd scripts/mc/D1_2420
+           ./subAna_D1_2420_707.sh
+           ;;
+
+    0.1.7) echo "Get samples -- extracting useful info: signal region and sideband region, ISR factors..."
            cd scripts/mc
            ./synthesizeD1_2420.sh
            ./getInfoD1_2420.sh
-           ./getFactorD1_2420.sh round3
+           ./getFactorD1_2420.sh round2
            ;;
 
-    0.1.6) echo "Get samples -- applying cuts: D1_2420 MC..."
+    0.1.8) echo "Get samples -- applying cuts: D1_2420 MC..."
            cd scripts/mc
            ./applyCutsD1_2420.sh
            ;;
@@ -91,33 +103,33 @@ case $option in
 
     0.2.1) echo "Fit distributions -- simultaneous fitting recoiling mass of D + Dmiss and recoiling mass of pipi..."
            cd scripts/ana
-           ./simul_fit.sh round3
+           ./simul_fit.sh round2
            ;;
 
     0.2.2) echo "Get shapes -- mixing MC shapes..."
            cd scripts/mc
-           ./mixMC.sh round3
+           ./mixMC.sh round2
            cd -
            cd scripts/ana
-           ./mixROOT.sh round3
+           ./mixROOT.sh round2
            ;;
 
     0.2.3) echo "Fit distributions -- fitting invariant mass of Kpipi..."
            cd scripts/ana
-           ./factorMKpipi.sh round3
+           ./factorMKpipi.sh round2
            ;;
 
     0.2.4) echo "Fit distributions -- fitting recoiling mass of Dpipi..."
            cd scripts/ana
-           ./fitRMDpipi.sh round3
+           ./fitRMDpipi.sh round2
            ;;
 
     0.2.5) echo "Calculate numbers -- calculating cross section of DDpipi and systerm uncertainties..."
            cd scripts/ana
-           ./calXS.sh round3
+           ./calXS.sh round2
            cd -
-           ./format_xs.py round3
-           ./cal_diff.py round3
+           ./format_xs.py round2
+           ./cal_diff.py round2
            ;;
 
 esac
